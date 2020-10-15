68844e891fece6e9fd2b54e203fbf55adee1f2bc
Andrey Tikhonov <tishka17@mail.ru>
2020-10-15 18:45:55 +0300

    topic page

--- questions/forms.py
+++ questions/forms.py
@@ -4,9 +4,12 @@ from .models import Topic
 
 
 class QuestionForm(forms.Form):
-    title = forms.CharField(label='Заголовок', max_length=80)
-    text = forms.CharField(label='Текст вопроса', max_length=4096, widget=forms.Textarea())
-    topic = forms.ModelChoiceField(queryset=Topic.objects.all(), empty_label="(Выберите тему)")
+    title = forms.CharField(label='Заголовок', max_length=80,
+                            widget=forms.TextInput(attrs={"class": "form-control"}))
+    text = forms.CharField(label='Текст вопроса', max_length=4096,
+                           widget=forms.Textarea(attrs={"class": "form-control"}))
+    topic = forms.ModelChoiceField(queryset=Topic.objects.all(), empty_label="(Выберите тему)",
+                                   widget=forms.Select(attrs={"class": "form-control"}))
 
 
 class AnswerForm(forms.Form):
